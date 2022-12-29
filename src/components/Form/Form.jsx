98299dd2bdc389543bda05995bb32283c2d33a7f
foundation23 <maksim.foundation@yandex.ru>
2022-12-30 00:01:48 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./Form.css"
+import {useTelegram} from "../../hooks/hooks";
 
 const Form = () => {
+    const [country, setCountry] = useState('');
+    const [sity, setSity] = useState('');
+    const [subject, setSubject] = useState('physical')
+    const {tg} = useTelegram()
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            tg: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!sity || !country) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [country, sity])
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+    const onChangeSity = (e) => {
+        setSity(e.target.value)
+    }
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
     return (
-        <div>
-Form
+        <div className={'form'}>
+            <h3> Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Город'}
+                value={sity}
+                onChange={onChangeSity}
+            />
+            <select
+                value={subject}
+                onChange={onChangeSubject}
+                className={'select'}
+            >
+
+                <option value={'physical'}>Физ.лицо</option>
+                <option value={'legal'}>Юр.лицо</option>
+            </select>
         </div>
     );
 };
