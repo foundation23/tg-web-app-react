5114e1f245df573646c4ef257fa0788fd47fea48
foundation23 <maksim.foundation@yandex.ru>
2023-01-02 12:52:46 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -48,7 +48,13 @@ const ProductList = () => {
 
     return (
         <div className={'list'}>
-            1233
+            {products.map(item => {
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            })}
         </div>
     );
 };
